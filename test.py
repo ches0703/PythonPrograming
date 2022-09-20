2cc4403af89e440673e1d07874500455613b5ef5
ches0703 <ches0703@naver.com>
2022-09-21 00:51:55 +0900

    testng add

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 # This is test file
 
-# 1 = mon, 2 = tue
-
-# 1년 > mon, 2년 tue...
 
 # 서기 일년부터 계산하여 2022년의 시작 연도를 계산하는 프로그렘
+# 1 = mon, 2 = tue
+# 1년 > mon, 2년 tue...
 
-count = 1
-for i in range(1, 2022):
-    if ((i % 4 == 0) and (i % 100 != 0) or (i % 400 == 0)):
-        count += 2
-    else:
-        count += 1
-count %= 7
-print(count)
+# count = 1
+# for i in range(1, 2022):
+#     if ((i % 4 == 0) and (i % 100 != 0) or (i % 400 == 0)):
+#         count += 2
+#     else:
+#         count += 1
+# count %= 7
+# print(count)
+
+
+# HomeWork01 test
+
+a = input()
+a = int(a, 16)
+print("%d" % (a))
+print(format(16, '#b'))
+print('{:#b}'.format(10))
